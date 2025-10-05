56ad962ef63df0a73f75b37572035d1711fed53a
m.jilevskiy <m.jilevskiy@aerodisk.ru>
2025-10-05 20:07:29 +0300

    feat: add GitHub Pages deployment

--- src/entities/cocktails/api/api.ts
+++ src/entities/cocktails/api/api.ts
@@ -1,14 +1,14 @@
-import { axiosInstance } from "@/shared";
-import { CocktailResponse } from "./types";
+import { axiosInstance } from '@/shared'
+import { CocktailResponse } from './types'
 
 export const cocktailsApi = {
   getCocktails: async (code: string): Promise<CocktailResponse> => {
-    const response = await axiosInstance.get<CocktailResponse>("/api/json/v1/1/search.php", {
+    const response = await axiosInstance.get<CocktailResponse>('/api/json/v1/1/search.php', {
       params: {
         s: code,
       },
-    });
-    
-    return response.data;
+    })
+
+    return response.data
   },
-};
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/coctail-test-task/',
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
